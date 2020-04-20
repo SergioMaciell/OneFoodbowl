d2d4515c565dd37e9dfa5fa0b6bdb5d3d05fc64d
Mario Jair Pineiro Bazan <mariojair.pineiro@my.uvm.edu.mx>
2020-04-20 04:59:17 -0500

    Modificación de Datacontext, creación del controlador, y vistas, modificación de botones con bootstrap

--- OneFoodBowl.web/Data/DataContext.cs
+++ OneFoodBowl.web/Data/DataContext.cs
@@ -4,8 +4,8 @@
 using OneFoodBowl.web.Data.Entities;
     public class DataContext : DbContext
     {
-        DbSet<Gender> Genders { get; set; }
-        DbSet<Customer> Customers { get; set; }
+       public DbSet<Gender> Genders { get; set; }
+       public DbSet<Customer> Customers { get; set; }
         public DataContext(DbContextOptions<DataContext>options):base(options)
         {
 
