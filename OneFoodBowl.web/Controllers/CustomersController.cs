a774797a7bd6468758ed186ec117227781e89aeb
sergio.maciell15@outlook.es <sergio.maciell15@outlook.es>
2020-04-20 15:32:02 -0500

    Creación control customers

---
+++ OneFoodBowl.web/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿namespace OneFoodBowl.web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using OneFoodBowl.web.Data;
+    using System.Threading.Tasks;
+
+    public class CustomersController : Controller
+    {
+        private readonly DataContext dataContext;
+
+        public CustomersController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return  View(await dataContext.Customers.ToListAsync()); ;
+        }
+    }
+}
